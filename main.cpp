bb864a8f2157276d2a1a644966dbae3664f2a229
visionsmika-gif <visionsmika@gmail.com>
2025-11-11 15:44:18 -0800

    update

--- main.cpp
+++ main.cpp
@@ -9,11 +9,13 @@ using namespace std;
 const int INITIAL_SIZE = 2;
 
 void printCars(const deque<Car> cars) {
+	cout << "Queue:\n";
 	if (cars.empty()) {
-		cout << "Empty\n";
+		cout << "\tEmpty\n";
 		return;
 	}
 	for (Car car : cars) {
+		cout << "\t";
 		car.print();
 	}
 }
@@ -21,10 +23,10 @@ void printCars(const deque<Car> cars) {
 int main() {
 	srand(time(0));
 
-	// Declare a deque of cars
+	// Declare a deque of cars.
 	deque<Car> cars;
 
-	// Initialize deque with 2 cars
+	// Initialize deque with 2 cars.
 	cout << "Initial queue:\n";
 	for (int i = 0; i < INITIAL_SIZE; ++i) {
 		cars.push_back(Car());
@@ -33,13 +35,17 @@ int main() {
 	}
 
 	int time = 1;
+	// Run a loop until the deque is empty.
 	while (!cars.empty()) {
+		bool somethingHappened = false;
+
 		// 55% probability that the car at the head of the line pays its toll and leaves the toll booth
 		int probability = rand() % 100 + 1;
 		if (probability <= 55) {
 			cout << "Time: " << time << " Operation: Car paid: ";
 			cars.front().print();
 			cars.pop_front();
+			somethingHappened = true;
 		}
 
 		// 45% probability that another car joins the line for the doll booth
@@ -48,10 +54,11 @@ int main() {
 			cars.push_back(Car());
 			cout << "Time: " << time << " Operation: Joined lane: ";
 			cars.back().print();
+			somethingHappened = true;
 		}
 
+		// After each time period, display the queue
 		printCars(cars);
-
 		++time;
 	}
 
