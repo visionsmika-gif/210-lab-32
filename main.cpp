d241e548dee845d6b5959d06bc28b44a1a38cab7
visionsmika-gif <visionsmika@gmail.com>
2025-11-12 16:40:39 -0800

    update

--- main.cpp
+++ main.cpp
@@ -14,12 +14,18 @@ const int INITIAL_SIZE = 2;
 const int NUM_LANES = 4;
 const int NUM_SIMS = 20;
 
+const int PROB_EMPTY_JOIN = 50;
 const int PROB_PAY = 46;
 const int PROB_JOIN = 39;
 const int PROB_SHIFT = 15;
 
 // Function to print a deque of cars.
 void printCars(array<deque<Car>, NUM_LANES> lanes);
+
+// Functions for the simulation events.
+void carJoins(array<deque<Car>, NUM_LANES>& lanes, int i);
+void carPays(array<deque<Car>, NUM_LANES>& lanes, int i);
+void carShifts(array<deque<Car>, NUM_LANES>& lanes, int i);
 
 int main() {
 	srand(time(0));
@@ -44,40 +50,45 @@ int main() {
 
 		for (int i = 0; i < NUM_LANES; ++i) {
 
-			// TODO: If a lane is currently empty but there is still more time in the simulation, those probabilities will be just 50/50 if a new car enters the queue or not. 
-
-
-			// 46% probability that the car at the head of the queue pays its toll and leaves the queue.
-			int probability = rand() % 100 + 1;
-			if (probability <= PROB_PAY && !lanes[i].empty()) {
-				cout << "Lane " << i + 1 << " Paid: ";
-				lanes[i].front().print();
-				lanes[i].pop_front();
+			// If a lane is empty, there is a 50% chance that a car will join. 
+			if (lanes[i].empty()) {
+				int probability = rand() % 100 + 1;
+				if (probability <= PROB_EMPTY_JOIN) {
+					carJoins(lanes, i);
+				}
 			}
-			// 39% probability that another car joins the queue.
-			else if (probability <= PROB_PAY + PROB_JOIN) {
-				lanes[i].push_back(Car());
-				cout << "Lane " << i + 1 << " Joined: ";
-				lanes[i].back().print();
-			}
-			// 15% probability that the rear car will shift lanes.
-			else if (!lanes[i].empty()) {
-				// Remove car from current lane.
-				Car shiftingCar = lanes[i].back();
-				lanes[i].pop_back();
+			// If a lane is not empty, these events have a chance of happening:
+			else {
+				// 46% probability that the car at the head of the queue pays its toll and leaves the queue.
+				int probability = rand() % 100 + 1;
+				if (probability <= PROB_PAY) {
+					carPays(lanes, i);
+				}
+				// 39% probability that another car joins the queue.
+				else if (probability <= PROB_PAY + PROB_JOIN) {
+					lanes[i].push_back(Car());
+					cout << "Lane " << i + 1 << " Joined: ";
+					lanes[i].back().print();
+				}
+				// 15% probability that the rear car will shift lanes.
+				else {
+					// Remove car from current lane.
+					Car shiftingCar = lanes[i].back();
+					lanes[i].pop_back();
 
-				// Print action.
-				cout << "Lane " << i + 1 << " Switched: ";
-				shiftingCar.print();
+					// Print action.
+					cout << "Lane " << i + 1 << " Switched: ";
+					shiftingCar.print();
 
-				// Choose a new lane to enter.
-				int newLane;
-				do {
-					newLane = rand() % NUM_LANES;
-				} while (newLane == i);	// Ensure that the new lane does not equal the current lane.
+					// Choose a new lane to enter.
+					int newLane;
+					do {
+						newLane = rand() % NUM_LANES;
+					} while (newLane == i);	// Ensure that the new lane does not equal the current lane.
 
-				// Add the shifting car to the new lane.
-				lanes[newLane].push_back(shiftingCar);
+					// Add the shifting car to the new lane.
+					lanes[newLane].push_back(shiftingCar);
+				}
 			}
 		}
 
@@ -107,4 +118,34 @@ void printCars(array<deque<Car>, NUM_LANES> lanes) {
 			}
 		}
 	}
+}
+
+void carJoins(array<deque<Car>, NUM_LANES>& lanes, int i) {
+	lanes[i].push_back(Car());
+	cout << "Lane " << i + 1 << " Joined: ";
+	lanes[i].back().print();
+}
+
+void carPays(array<deque<Car>, NUM_LANES>& lanes, int i) {
+	cout << "Lane " << i + 1 << " Paid: ";
+	lanes[i].front().print();
+	lanes[i].pop_front();
+}
+void carShifts(array<deque<Car>, NUM_LANES>& lanes, int i) {
+	// Remove car from current lane.
+	Car shiftingCar = lanes[i].back();
+	lanes[i].pop_back();
+
+	// Print action.
+	cout << "Lane " << i + 1 << " Switched: ";
+	shiftingCar.print();
+
+	// Choose a new lane to enter.
+	int newLane;
+	do {
+		newLane = rand() % NUM_LANES;
+	} while (newLane == i);	// Ensure that the new lane does not equal the current lane.
+
+	// Add the shifting car to the new lane.
+	lanes[newLane].push_back(shiftingCar);
 }
