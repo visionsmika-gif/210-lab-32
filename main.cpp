f4578a9a58abbf0f83786e4cc201382c75953f4d
visionsmika-gif <visionsmika@gmail.com>
2025-11-12 16:07:47 -0800

    update

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@ using namespace std;
 
 const int INITIAL_SIZE = 2;
 const int NUM_LANES = 4;
+const int NUM_SIMS = 20;
+
+const int PROB_PAY = 46;
+const int PROB_JOIN = 39;
+const int PROB_SHIFT = 15;
 
 // Function to print a deque of cars.
 void printCars(array<deque<Car>, NUM_LANES> lanes);
@@ -22,7 +27,7 @@ int main() {
 	// Declare an array containing lanes.
 	array<deque<Car>, NUM_LANES> lanes;
 
-	// Initialize deque with 2 cars.
+	// Initialize the lanes with cars.
 	cout << "Initial queue:\n";
 	for (int i = 0; i < NUM_LANES; ++i) {
 		cout << "Lane " << i + 1 << "\n";
@@ -33,37 +38,46 @@ int main() {
 		}
 	}
 
-	cout << "\nOutput the current deque status per the sample output\n";
-	printCars(lanes);
+	// Run the simulation a number of times.
+	for (int time = 0; time < NUM_SIMS; ++time) {
+		cout << "Time: " << time + 1 << "\n";
 
-	/*
-	int time = 1;
-	// Run a loop until the deque is empty.
-	while (!cars.empty()) {
-		cout << "\n";
-		// 55% probability that the car at the head of the line pays its toll and leaves the toll booth.
-		int probability = rand() % 100 + 1;
-		if (probability <= 55) {
-			cout << "Time: " << time << " Operation: Car paid: ";
-			cars.front().print();
-			cars.pop_front();
+		for (int i = 0; i < NUM_LANES; ++i) {
+			// 46% probability that the car at the head of the queue pays its toll and leaves the queue
+			int probability = rand() % 100 + 1;
+			if (probability <= PROB_PAY) {
+				cout << "Lane " << i + 1 << " Paid: ";
+				
+				
+			}
+
+
+
+			/*
+			// 55% probability that the car at the head of the line pays its toll and leaves the toll booth.
+			int probability = rand() % 100 + 1;
+			if (probability <= 55) {
+				cout << "Time: " << time << " Operation: Car paid: ";
+				cars.front().print();
+				cars.pop_front();
+			}
+
+			// 45% probability that another car joins the line for the doll booth.
+			// The 'else' branch accounts for the chance that the probability was > 55 (accounting for the other 45 numbers out of 100)
+			// NOTE: I assumed that only one operation could happen per cycle because the sample output never shows two operations happening
+			// in the same cycle, so I used an if-else branch.
+			else {
+				cars.push_back(Car());
+				cout << "Time: " << time << " Operation: Joined lane: ";
+				cars.back().print();
+			}
+
+			// After each time period, display the queue.
+			printCars(cars);
+			++time;
+			*/
 		}
-
-		// 45% probability that another car joins the line for the doll booth.
-		// The 'else' branch accounts for the chance that the probability was > 55 (accounting for the other 45 numbers out of 100)
-		// NOTE: I assumed that only one operation could happen per cycle because the sample output never shows two operations happening
-		// in the same cycle, so I used an if-else branch.
-		else {
-			cars.push_back(Car());
-			cout << "Time: " << time << " Operation: Joined lane: ";
-			cars.back().print();
-		}
-
-		// After each time period, display the queue.
-		printCars(cars);
-		++time;
 	}
-	*/
 
 	return 0;
 }
